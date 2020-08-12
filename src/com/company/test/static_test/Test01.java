cb4561c69ed51657f1f589c55cdf63c32e0455de
Amorend <am16@vip.qq.com>
2020-08-12 18:01:25 +0800

    默认更改列表

--- src/com/company/test/static_test/Test01.java
+++ src/com/company/test/static_test/Test01.java
@@ -3,6 +3,23 @@ package com.company.test.static_test;
 public class Test01 {
 
 }
+ class Threads4{
+    public static void main(String[] args){
+        new Threads4().go();
+    }
+    public void go(){
+        Runnable r=new Runnable(){
+            public void run(){
+                System.out.print("foo");
+            }
+        };
+        Thread t=new Thread(r);
+        t.start();
+    }
+}
+
+
+
 class X{
     Y y=new Y();
     public X(){
