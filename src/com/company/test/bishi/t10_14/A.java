7fc89b1d0d5e876c2e18dde02bf814de3f153460
Amorend <am16@vip.qq.com>
2020-10-16 21:30:02 +0800

    默认更改列表

---
+++ src/com/company/test/bishi/t10_14/A.java
@@ -0,0 +1,15 @@
+package com.company.test.bishi.t10_14;
+import java.util.*;
+
+public class A {
+
+}
+
+class Test {
+    public static void main(String[] args){
+        List l = new ArrayList();
+        l.add("A");
+        l.add(0,"B");
+        System.out.print(l.get(0));
+    }
+}
---
+++ src/com/company/test/bishi/t10_16/A.java
@@ -0,0 +1,15 @@
+package com.company.test.bishi.t10_16;
+
+import java.util.*;
+
+public class A {
+    void exe2(){
+        List l = new ArrayList<String>();
+        l.add(122);
+        System.out.println(l);
+    }
+
+    public static void main(String[] args) {
+        new A().exe2();
+    }
+}
