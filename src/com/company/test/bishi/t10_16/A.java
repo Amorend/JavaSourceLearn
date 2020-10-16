57e035333a5dc82031d55fe6e3cef565b5e81343
Amorend <am16@vip.qq.com>
2020-10-16 21:54:58 +0800

    默认更改列表

--- src/com/company/test/bishi/t10_16/A.java
+++ src/com/company/test/bishi/t10_16/A.java
@@ -1,15 +1,41 @@
 package com.company.test.bishi.t10_16;
 
 import java.util.*;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class A {
-    void exe2(){
-        List l = new ArrayList<String>();
-        l.add(122);
-        System.out.println(l);
+    public static Set<String> set = new TreeSet<String>();
+
+    public static void doSet(String start, String[] sourceList, int max) {
+        String[] olds = start.split("_");
+        if (olds.length == max) {
+            set.add(start.replaceAll("_", "").trim());
+        } else {
+            for (int s = 0; s < sourceList.length; s++) {
+                if (Arrays.asList(olds).contains(sourceList[s])) {
+                    continue;
+                } else {
+                    doSet(start + "_" + sourceList[s], sourceList, max);
+                }
+            }
+        }
+    }
+    public static void doSet(String[] sourceList, int max) {
+        for (int start = 0; start < sourceList.length; start++) {
+            doSet(sourceList[start], sourceList, max);
+        }
+
     }
 
     public static void main(String[] args) {
-        new A().exe2();
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String[] haha = a.split("");
+        doSet(haha, 3);
+        System.out.println(set.size());
     }
 }
+
